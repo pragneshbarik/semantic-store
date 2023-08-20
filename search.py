ce680a6419905ef165ac0996f24d41dc793c160c
pragneshbarik <barikpragnesh@gmail.com>
2023-08-20 22:14:47 +0530

    initial commit

---
+++ search.py
@@ -0,0 +1,10 @@
+import os
+
+# Tasks
+# 1. Install Anaconda, Pytorch,
+# 2. Make a function to list all the images in directory
+
+
+def list_images(path):
+    # this lists all files in a directory
+    return os.listdir(path)
