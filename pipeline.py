c503da62abac06c2ccd27b2d1ec357f879bf777f
pragneshbarik <barikpragnesh@gmail.com>
2023-09-01 17:49:15 +0530

    Made image pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,5 @@
 import glob
+import whisper
 import re
 import torch
 import numpy as np
@@ -31,6 +32,16 @@ class Pipeline(ABC):
     def similarity_search(self, query: str, k: int) -> list[int] :
         pass
 
+    @staticmethod
+    def order_by(records, order):
+        if not all(0 <= i < len(records) for i in order) or len(set(order)) != len(order):
+            raise ValueError("Invalid order list")
+
+        record_dict = {record[0]: record for record in records}
+
+        ordered_records = [record_dict[i] for i in order]
+
+        return ordered_records
 class TextPipeline(Pipeline):
     def __init__(self, faiss_uri: str, sqlite_uri: str) -> None :
 
@@ -136,9 +147,10 @@ class TextPipeline(Pipeline):
 
         Q = f"SELECT * FROM text_table WHERE faiss_id in ({','.join(map(str, faiss_indices))})"
 
-        return self.db.execute(Q).fetchall() , D
+        return Pipeline.order_by(self.db.execute(Q).fetchall(), I[0]) , D
 
 class ImagePipeline(Pipeline) :
+
     def __init__(self, faiss_uri: str, sqlite_uri: str) -> None :
         self.db_connection = sqlite3.connect(sqlite_uri)
         self.db = self.db_connection.cursor()
@@ -148,7 +160,105 @@ class ImagePipeline(Pipeline) :
             CREATE TABLE IF NOT EXISTS image_table (
                 faiss_id INTEGER PRIMARY KEY,
                 file_id TEXT,
+                path TEXT
+            )
+        ''')
+
+        self.db_connection.commit()
+        
+        try :
+            self.index = faiss.read_index(faiss_uri)
+        
+        except RuntimeError:
+            self.index = faiss.IndexFlatL2(512)
+            faiss.write_index(self.index, faiss_uri)
+
+        self.faiss_uri = faiss_uri
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model, self.preprocess = clip.load("ViT-B/32", device=self.device)
+
+  
+
+    def commit(self) :
+        self.db_connection.commit()
+        faiss.write_index(self.index, self.faiss_uri)
+
+
+    def insert_file(self, path: str) -> tuple :
+        file_id = str(uuid.uuid4())
+        first_index = self.index.ntotal
+        embeddings = {}
+        with torch.no_grad() :
+            image = self.preprocess(Image.open(path)).unsqueeze(0).to(self.device)
+            embeddings[path] = self.model.encode_image(image)
+            
+        embed_vector = embeddings[path].detach().cpu().numpy()
+
+        embed_vector = np.float32(embed_vector)
+        
+        faiss.normalize_L2(embed_vector)
+        self.index.add(embed_vector)
+        
+        
+
+        self.db.execute(
+           "INSERT INTO image_table (faiss_id, file_id, path) VALUES (?, ?, ?)", 
+           (first_index, file_id, path)
+        )
+
+        self.commit()
+
+        return file_id, first_index
+
+
+    def similarity_search(self, q: str, k: int, file: bool = False) -> list[int]:
+        if not file:
+            tokens = clip.tokenize([q]).to(self.device)
+            text_features = self.model.encode_text(tokens)
+
+            text_np = text_features.detach().cpu().numpy()
+            text_np = np.float32(text_np)
+
+            faiss.normalize_L2(text_np)
+
+            D, I = self.index.search(text_np, k)
+
+            faiss_indices = list(I[0])
+            print(D, I)
+
+            Q = f"SELECT * FROM image_table WHERE faiss_id in ({','.join(map(str, faiss_indices))})"
+
+
+
+            return Pipeline.order_by(self.db.execute(Q).fetchmany(k), I[0]), D
+
+        else:
+            file_ext = q.split('.')[-1]
+            if file_ext.lower() in ['png', 'jpg', 'jpeg']:
+                image = self.preprocess(Image.open(q)).unsqueeze(0).to(self.device)
+            
+                embeddings = self.model.encode_image(image)
+                
+                D, I = self.index.search(embeddings, k)
+                faiss_indices = list(I[0])
+
+                Q = f"SELECT * FROM image_table WHERE faiss_id in ({','.join(map(str, faiss_indices))})"
+                
+
+                return self.db.execute(Q).fetchall(), D
+
+class AudioPipeline(Pipeline):
+    def __init__(self, faiss_uri: str, sqlite_uri: str) -> None :
+        self.db_connection = sqlite3.connect(sqlite_uri)
+        self.db = self.db_connection.cursor()
+        
+        
+        self.db.execute('''
+            CREATE TABLE IF NOT EXISTS text_table (
+                faiss_id INTEGER PRIMARY KEY,
+                file_id TEXT,
                 path TEXT,
+                text_data TEXT        
             )
         ''')
 
@@ -162,62 +272,57 @@ class ImagePipeline(Pipeline) :
             faiss.write_index(self.index, faiss_uri)
 
         self.faiss_uri = faiss_uri
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model, self.preprocess = clip.load("ViT-B/32", device=self.device)
-
+        self.whisper_model = whisper.load_model('tiny.en')
+        self.model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+    
     def commit(self) :
         self.db_connection.commit()
         faiss.write_index(self.index, self.faiss_uri)
 
 
-    def insert_file(self, path: str) -> tuple :
-        image = self.preprocess(Image.open(path)).unsqueeze(0).to(self.device)
-        file_id = str(uuid.uuid4())
-        first_index = self.index.ntotal
-        with torch.no_grad() :
-            embeddings = self.model.encode_image(image)
-            self.index.add(embeddings)
+    def encode_text(self, sentences: list[str]) -> list[torch.Tensor] | np.ndarray | torch.Tensor :
+        embeddings = self.model.encode(sentences)
+        return np.array(embeddings).reshape(-1, 384)
 
-        self.db.execute(
-           f"INSERT INTO image_table (faiss_id, file_id, path) VALUES ({first_index}, {file_id}, {path})", 
-        )
+    def insert_file(self, path: str) -> tuple :
+        extracted_text = self.model.transribe(path)
+        extracted_text = extracted_text['text']
+
+        sentences = TextPipeline.split_text(extracted_text)
+        file_id = str(uuid.uuid4())
+        embeddings = self.encode_text(sentences)
+        first_index = self.index.ntotal
+        self.index.add(embeddings)
+        last_index = self.index.ntotal
+
+        for i, sentence in enumerate(sentences) :
+            self.db.execute(
+                "INSERT INTO text_table (faiss_id, file_id, path, text_data) VALUES (?, ?, ?, ?)",
+                (first_index + i, file_id, path, sentence)
+            )
 
         self.commit()
-
-        return file_id, first_index
-
-
-    def similarity_search(self, q: str, k: int, file: bool = False) -> list[int]:
-        if not file :
-            text = clip.tokenize([q]).to(self.device)
-            text_embeds = self.model.encode_text(text)
-
-            D, I = self.index.search(text_embeds, k)
-
-            faiss_indices = list(I[0])
-
-            Q = f"SELECT * FROM image_table WHERE faiss_id in ({','.join(map(str, faiss_indices))})"
-            return self.db.execute(Q).fetchall(), D
+        return file_id, first_index, last_index
+    
+    def similarity_search(self, query: str, k: int) -> list[int]:
+        '''
+        Returns list of (faiss_id, file_id, path, text_data) and Distances ranked according to distances.
         
-        else :
-            file_ext = q.split('.')[-1]
-            if file_ext.lower() in ['png', 'jpg', 'jpeg'] :
-               image = self.preprocess(Image.open(q)).unsqueeze(0).to(self.device)
-               with torch.no_grad() :
-                   embeddings = self.model.encode_image(image)
-                   D, I = self.index.search(embeddings, k)
-                   faiss_indices = list(I[0])
-                   
-                   Q = f"SELECT * FROM image_table WHERE faiss_id in ({','.join(map(str, faiss_indices))})"
-                   
-                   return self.db.execute(Q).fetchall(), D
-
-class AudioPipeline(Pipeline):
-    pass
+        '''
+        
+        query_embedding = self.model.encode([query])
+        D, I = self.index.search(np.array(query_embedding).reshape(-1, 384), k)
 
 
-class VideoPipeline(Pipeline):
-    pass
+        faiss_indices = list(I[0])
+        print(D, I)
+
+        Q = f"SELECT * FROM text_table WHERE faiss_id in ({','.join(map(str, faiss_indices))})"
+
+        return self.db.execute(Q).fetchall() , D
+
+# class VideoPipeline(Pipeline):
+#     pass
 class Store:
     def __init__(self) :
         self.__is_connected = False
@@ -232,13 +337,14 @@ class Store:
         text_index = sql_uri + '_text.faiss'
         image_index = sql_uri + '_image.faiss'
 
-        
 
 
 
 
 
-    def multimodal_search(self, path: str, k: int) :
+
+
+    def multimodal_search(self, path: str, k: int, left: str, right: str) :
         pass
     
     def search(self,) :
@@ -253,9 +359,13 @@ class Store:
 
 
 
-text_pipeline = TextPipeline(faiss_uri='text.faiss', sqlite_uri='semantic.db')
-# text_pipeline.insert_file('Problem Solving in Data Structures & Algorithms Using C ( PDFDrive ).pdf')
-print(text_pipeline.similarity_search("How to reverse a linked list", 10))
+# text_pipeline = TextPipeline(faiss_uri='text.faiss', sqlite_uri='semantic.db')
+image_pipeline = ImagePipeline(faiss_uri='image.faiss', sqlite_uri='semantic.db')
+
+# image_pipeline.insert_file('cat.jpg')
+# image_pipeline.insert_file('dog.jpeg')
+# image_pipeline.insert_file('sky.jpeg')
+print(image_pipeline.similarity_search("a black dog", 3))
 # print(text_pipeline.ntotal)
 # tick = time.time()
 # embeds = text_pipeline.encode_text(["There are 7 wonders in the world", "I have a ball, and will play with dog", "a great pizza"])
