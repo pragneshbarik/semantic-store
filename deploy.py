0cef9afe9646e6be62a9b776c5b6819457223bd4
Pragnesh Barik <65221256+pragneshbarik@users.noreply.github.com>
2023-09-18 18:20:45 +0530

    Delete deploy.py

--- deploy.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('python setup.py sdist bdist_wheel')
-os.system('twine upload --username __token__ --password pypi-AgEIcHlwaS5vcmcCJGI0ZDRkMzA1LTNhMjUtNDQxNC1iYTQ3LTNlMTdiYmNiNTgwZAACKlszLCIxOWUxYTc5Ny01OTU0LTQ5YzktYTk2Ni05YmFmYTU2ZjVlNjQiXQAABiCl39B04xKCIgxE4VVDpu-rmA0d2HRCrpCLj31uztrdYA dist/*')
